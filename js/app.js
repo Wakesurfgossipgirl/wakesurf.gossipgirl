2d8809ed7086a9260c1bac70aa0888110595747a
Wakesurfgossipgirl <wakesurf.gossipgirl@gmail.com>
2026-08-03 00:45:11 +0300

    Implement async form submission with fetch API
    Added asynchronous form submission with fetch API to send data to a specified URL and handle responses.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,16 @@
-// =======================
-// Загрузка страницы
-// =======================
-
 document.addEventListener("DOMContentLoaded", () => {
 
     const form = document.getElementById("gossipForm");
     const button = form.querySelector("button");
+
     const fileInput = document.getElementById("files");
     const uploadText = document.querySelector(".upload span");
-
-    // Показываем выбранные файлы
 
     fileInput.addEventListener("change", () => {
 
         if (fileInput.files.length === 0) {
 
             uploadText.textContent = "＋ Добавить фото или видео";
-
             return;
 
         }
@@ -24,7 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
         if (fileInput.files.length === 1) {
 
             uploadText.textContent = "✓ " + fileInput.files[0].name;
-
             return;
 
         }
@@ -34,7 +27,61 @@ document.addEventListener("DOMContentLoaded", () => {
 
     });
 
-    // Пока отправку просто имитируем
+    form.addEventListener("submit", async function (e) {
+
+        e.preventDefault();
+
+        button.disabled = true;
+        button.textContent = "ОТПРАВКА...";
+
+        const data = {
+
+            story: document.getElementById("story").value,
+            contact: document.getElementById("author").value
+
+        };
+
+        try {
+
+            const response = await fetch(
+                "https://script.google.com/macros/s/AKfycbyXQ1RF7csgqQ1Nj0gAEHHn1OdkyZpQc8cg-0MuSfuwhqVxrxJ61hDxhAfI9LuicWmHXA/exec",
+                {
+                    method: "POST",
+                    body: JSON.stringify(data)
+                }
+            );
+
+            if (!response.ok)
+                throw new Error();
+
+            alert(`Спасибо!
+
+Ваше сообщение получено.
+
+XOXO,
+ВЕЙКСЕРФ СПЛЕТНИЦА`);
+
+            form.reset();
+
+            uploadText.textContent =
+                "＋ Добавить фото или видео";
+
+        }
+
+        catch {
+
+            alert("Ошибка отправки. Попробуйте еще раз.");
+
+        }
+
+        button.disabled = false;
+
+        button.textContent =
+            "♡ ОТПРАВИТЬ АНОНИМНО";
+
+    });
+
+});    // Пока отправку просто имитируем
 
     form.addEventListener("submit", function (e) {
 
